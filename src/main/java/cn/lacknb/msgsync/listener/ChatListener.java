6f8857f1e679b849e8ef0dd3f90cb8b83bf5a67b
BuildTools <unconfigured@null.spigotmc.org>
2023-07-25 20:36:26 +0800

    add - 加入游戏、退出游戏事件通知

--- src/main/java/cn/lacknb/msgsync/listener/ChatListener.java
+++ src/main/java/cn/lacknb/msgsync/listener/ChatListener.java
@@ -2,36 +2,73 @@ package cn.lacknb.msgsync.listener;
 
 import cn.lacknb.msgsync.MsgSyncQq;
 import cn.lacknb.msgsync.util.MyHttpClient;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ChatListener implements Listener {
 
     private static final Logger log = Logger.getLogger("ChatListener");
 
-    private MsgSyncQq plugin;
+    /**
+     * 发送消息的api
+     */
+    private final String api;
+
+    /**
+     * 消息前缀
+     */
+    private final String msgPrefix;
+
+    /**
+     * 配置文件
+     */
+    private final FileConfiguration config;
+
+    /**
+     * api 请求体
+     */
+    private final String requestBody;
 
     public ChatListener(MsgSyncQq msgSyncQq) {
-        this.plugin = msgSyncQq;
+        this.config = msgSyncQq.getConfig();
+        this.api = config.getString("api-url");
+        this.msgPrefix = config.getString("msg-prefix");
+        this.requestBody = config.getString("request-body");
     }
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         String message = event.getMessage();
         //todo 敏感信息过滤
-
         String name = event.getPlayer().getName();
-        FileConfiguration config = plugin.getConfig();
-        String api = config.getString("api-url");
-        String msgPrefix = config.getString("msg-prefix");
-        String requestBody = config.getString("request-body");
-        String content = msgPrefix + name + ": " + message;
-        assert requestBody != null;
-        String replace = requestBody.replace("{msg}", content);
+        String content = name + ": " + message;
+        sendMessage(content);
+    }
+
+    @EventHandler
+    public void onPLayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        List<String> playerList = Bukkit.getServer().getOnlinePlayers().stream().map(Player::getName)
+                .collect(Collectors.toList());
+        String builder = "玩家：" + playerName + " 加入了游戏\n" +
+                "\n当前在线玩家：\n" +
+                String.join("\n ", playerList);
+        sendMessage(builder);
+    }
+
+    private void sendMessage(String msg) {
+        msg = msgPrefix + msg;
+        String replace = requestBody.replace("{msg}", msg);
         try {
             String post = MyHttpClient.post(api, replace);
             log.info(post);
@@ -39,7 +76,17 @@ public class ChatListener implements Listener {
             e.printStackTrace();
             log.info("msg send error !");
         }
+    }
 
+    @EventHandler
+    public void onPLayerQuit(PlayerQuitEvent event) {
+        String playerName = event.getPlayer().getName();
+        List<String> playerList = Bukkit.getServer().getOnlinePlayers().stream().map(Player::getName)
+                .collect(Collectors.toList());
+        String builder = "玩家：" + playerName + " 退出了游戏\n" +
+                "\n当前在线玩家：\n" +
+                String.join("\n ", playerList);
+        sendMessage(builder);
     }
 
 }
